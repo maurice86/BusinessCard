5cca2b426065e7f780981d9bc599a3418d94bf60
langymaurice <l.simpsonhandy@gmail.com>
2021-05-25 09:54:37 -0700

    Initial Commit

---
+++ LangfordCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LangfordCard
+//
+//  Created by Langy Maurice on 5/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
